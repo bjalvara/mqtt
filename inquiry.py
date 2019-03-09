b8539b61ef94f0c53a4f4a380eeace053932fa31
bjalvara <46462045+bjalvara@users.noreply.github.com>
2019-03-08 23:30:45 -0800

    added inquiry.py

---
+++ inquiry.py
@@ -0,0 +1,16 @@
+#performs a simple device inquiry followed by a remote name of each discovered  device
+
+import bluetooth
+
+print("performing inquiry...")
+nearby_devices = bluetooth.discover_devices(
+    duration=8, lookup_names=True, flush_cache=True, lookup_class=False
+)
+
+print("found %d devices" % len(nearby_devices))
+
+for addr, name in nearby_devices:
+    try:
+        print(" %s - %s" % (addr, name))
+    except UnicodeDecodeError:
+        print(" %s -%s" % (addr,name.encode('utf-8', 'replace')))
--- test_publish.py
+++ test_publish.py
@@ -1,8 +1,8 @@
 import paho.mqtt.publish as publish
 
 # Keep in mind the correct ip address
-MQTT_SERVER = "192.168.137.197"
+MQTT_SERVER = "192.168.137.124"
 MQTT_PATH = "test_channel"
 
 
-publish.single(MQTT_PATH, "LED off", hostname=MQTT_SERVER)
+publish.single(MQTT_PATH, "LED on", hostname=MQTT_SERVER)
